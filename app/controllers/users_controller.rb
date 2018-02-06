71b5af20d68dc3ff676303d6d66b328035ad50c2
nhdong13 <nhatdong1301@gmail.com>
2018-02-06 17:30:04 +0700

    all

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,21 @@ def destroy
     @user = User.find(params[:id])
   end
 
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
 
     def user_params
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
   test "should get new" do
     get users_new_url
     assert_response :success
   end
+
 
 end
